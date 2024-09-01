cb5cbeaa3f7a94c713711e87a2d5f05f9a64a8ec
yakov <yakovglee@gmail.com>
2024-09-01 22:20:04 +0900

    upd: getters url

--- front/src/service/getters.js
+++ front/src/service/getters.js
@@ -1,9 +1,5 @@
-const API_KEY = 'AIzaSyDT2sVolBj1PhM3iyn74-FBlGD9isyLyjc'; 
-const SHEET_ID = '1NmPGcwQ67FebYd3McN4lf7jcN2hKcmM6W1cpBRcQZa4'; 
-
 export const fetchSheetData = async (SHEET_NAME='word') => {
-    const RANGE = `${SHEET_NAME}!A:E`; 
-    const url = `https://sheets.googleapis.com/v4/spreadsheets/${SHEET_ID}/values/${RANGE}?key=${API_KEY}`;
+    const url = 'https://thejustiks.pythonanywhere.com/';
     
     try {
         const response = await fetch(url);
@@ -12,20 +8,8 @@ export const fetchSheetData = async (SHEET_NAME='word') => {
         }
         
         const data = await response.json();
-        const values = data.values;
-        
-        const headers = values[0];
-        const rows = values.slice(1);
-
-        const jsonData = rows.map(row => {
-            const rowData = {};
-            headers.forEach((header, index) => {
-                rowData[header] = row[index] || ''; 
-            });
-            return rowData;
-        });
-        
-        return jsonData;
+            
+        return data.data;
     } catch (error) {
         console.error('Error fetching data:', error);
         return [];
