34f11aad084d9d54b7115695d869b57ef917d8ae
Яков <yakovglee@gmai.com>
2024-09-21 16:00:17 +0900

    add reverse word

--- front/src/ui/NewCard.js
+++ front/src/ui/NewCard.js
@@ -1,3 +1,5 @@
+import { useState } from 'react';
+
 import { Card, Carousel } from 'antd';
 
 import './styles/NewCard.css'
@@ -5,14 +7,29 @@ import './styles/NewCard.css'
 
 const Index = ({data}) => {
 
+    const [wordId, setWordId] = useState(0);
+    const [isRevere, setisRevere] = useState(false);
+
+    const handleCarouselChange = (current) => {
+        setWordId(2 * current);
+        setisRevere(false);
+    };
+
+    const handleChangeReverse = () => {
+        setisRevere(prev => !prev);
+    };
+    
+
     return (
         <div className='custom-card'>
             <Card>
-                <p className="textTitle">{data.word}</p>
+                <p className="textTitle"  onClick={handleChangeReverse}>{ isRevere ? data.trans_word[wordId] : data.word }</p>
                 
                     <div className="dfn_content">
-                        <Carousel dots={true}>
+                        <Carousel dots={true} afterChange={handleCarouselChange}>
                             {data.dfn.map((dfn, idx) => (
+
+
                                 <div key={`definition-${idx}`} className='carousel-item'>
                                     <ul>
                                         <li className='main-dfn'>{dfn}</li>
