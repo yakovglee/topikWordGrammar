8d43f67d45f32d06e7e2db64aa3c13e76b3fc03d
Яков <yakovglee@gmai.com>
2024-09-21 17:16:17 +0900

    some fix

--- front/src/App.js
+++ front/src/App.js
@@ -1,9 +1,9 @@
 import './App.css';
 import React, { useState, useEffect } from 'react';
-import { Spin } from 'antd';
+import { Spin} from 'antd';
 
 import { fetchSheetData } from './service/getters';
-import { tabsLvl, tabsPart, data } from './service/data.js';
+import { tabsLvl, tabsPart } from './service/data.js';
 
 import CustomTabs from './ui/CustomTabs.js';
 import NewCard from './ui/NewCard.js';
