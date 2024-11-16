0c9e33ca3dff958a24bc27473230ce33b83e59fa
yakovglee <yakovglee@gmai.com>
2024-11-16 10:35:57 +0900

    add: flask-sqla

--- back/flask_app.py
+++ back/flask_app.py
@@ -1,12 +1,8 @@
 from flask import Flask, jsonify, request
 from flask_cors import CORS
-
 from config import config
 from getters import get_word_from_gs
-
 from datetime import datetime
-
-from const import DATA
 
 ORIGINS = config["ORIGINS"]
 
@@ -20,14 +16,12 @@ def api():
     partOfSpeach = args.get("pos", default="명사", type=str)
     level = args.get("level", default=1, type=int)
     wordCount = args.get("wordCount", default=5, type=int)
-    global DATA
 
     current_date = datetime.now().strftime("%Y-%m-%d")
 
-    DATA["date"] = current_date
-    DATA["word"] = get_word_from_gs(pos=partOfSpeach, level=level, wordCount=wordCount)
+    word = get_word_from_gs(pos=partOfSpeach, level=level, wordCount=wordCount)
 
-    return jsonify(DATA)
+    return jsonify(word)
 
 
 if __name__ == "__main__":
