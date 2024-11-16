33d6be906dbb5cd52d497c3d70ca3c2515a99d01
yakovglee <yakovglee@gmai.com>
2024-11-16 21:51:06 +0900

    upd: handle to change nav

--- front/src/widget/LvlBar/ui/LvlBar.tsx
+++ front/src/widget/LvlBar/ui/LvlBar.tsx
@@ -2,6 +2,16 @@ import { CustomTabs } from "../../../shared/CustomTabs";
 import { tabsLvl } from "../data/data";
 import "./LvlBar.scss";
 
-export const LvlBar = () => {
-    return <CustomTabs items={tabsLvl} styled_class_name="lvlbar-tabs" />;
+interface LvlBarProps {
+    onChange: (lvl: string) => void;
+}
+
+export const LvlBar = ({ onChange }: LvlBarProps) => {
+    return (
+        <CustomTabs
+            items={tabsLvl}
+            styled_class_name="lvlbar-tabs"
+            onChange={onChange}
+        />
+    );
 };
--- front/src/widget/PartBar/ui/PartBar.tsx
+++ front/src/widget/PartBar/ui/PartBar.tsx
@@ -2,6 +2,16 @@ import { CustomTabs } from "../../../shared/CustomTabs";
 import { tabsPart } from "../data/data";
 import "./PartBar.scss";
 
-export const PartBar = () => {
-    return <CustomTabs items={tabsPart} styled_class_name="tabbar-tabs" />;
+interface PartBarProps {
+    onChange: (key: string) => void;
+}
+
+export const PartBar = ({ onChange }: PartBarProps) => {
+    return (
+        <CustomTabs
+            items={tabsPart}
+            styled_class_name="tabbar-tabs"
+            onChange={onChange}
+        />
+    );
 };
