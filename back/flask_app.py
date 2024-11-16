8ce5c2a79b7ca8500fc2f94b6e96d03675b741a5
yakovglee <yakovglee@gmai.com>
2024-11-16 15:01:50 +0900

    upd: a main app

--- back/flask_app.py
+++ back/flask_app.py
@@ -2,11 +2,10 @@ from flask import Flask, jsonify, request
 from flask_cors import CORS
 
 from config import config
-from getters import get_word_from_gs
-from datetime import datetime
+from getters import create_words
+from db_crud import get_words
 
 from model import db
-
 
 ORIGINS = config["ORIGINS"]
 DATABASE_URI = config["DATABASE_URI"]
@@ -15,7 +14,7 @@ DATABASE_URI = config["DATABASE_URI"]
 app = Flask(__name__)
 
 
-CORS(app, resources={r"/api/*": {"origins": ORIGINS}})
+CORS(app, resources={r"/*": {"origins": ORIGINS}})
 
 app.config["SQLALCHEMY_DATABASE_URI"] = DATABASE_URI
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
@@ -25,18 +24,37 @@ with app.app_context():
     db.create_all()
 
 
-@app.route("/api/", methods=["GET"])
+@app.route("/", methods=["GET"])
 def api():
     args = request.args
     partOfSpeach = args.get("pos", default="명사", type=str)
     level = args.get("level", default=1, type=int)
     wordCount = args.get("wordCount", default=5, type=int)
 
-    current_date = datetime.now().strftime("%Y-%m-%d")
+    words = get_words(lvl=level, pos=partOfSpeach)
 
-    word = get_word_from_gs(pos=partOfSpeach, level=level, wordCount=wordCount)
+    if not words:
+        create_words(partOfSpeach, level, wordCount)
+        words = get_words(lvl=level, pos=partOfSpeach)
 
-    return jsonify(word)
+    return jsonify(
+        [
+            {
+                "id": word.id,
+                "word": word.word,
+                "lvl": word.lvl,
+                "pos": word.pos,
+                "link": word.link,
+                "dfn": word.dfn,
+                "dfn_ru": word.dfn_ru,
+                "dfn_en": word.dfn_en,
+                "word_ru": word.word_ru,
+                "word_en": word.word_en,
+                "tags": word.tags,
+            }
+            for word in words
+        ]
+    )
 
 
 if __name__ == "__main__":
