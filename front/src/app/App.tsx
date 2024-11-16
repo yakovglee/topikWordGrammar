bb3a893baffb2ca2db052754c9c7d35f59ab3166
yakovglee <yakovglee@gmai.com>
2024-11-16 21:56:42 +0900

    main app

--- front/src/app/App.tsx
+++ front/src/app/App.tsx
@@ -1,23 +1,62 @@
 import "./App.scss";
-
+import { useState, useEffect } from "react";
 import { CardWord, LvlBar, PartBar } from "../widget";
-
-import { data } from "../widget/CardWord/data/data";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-
-const queryClient = new QueryClient();
+import { TWordData } from "../widget/CardWord/data/types";
+import { getData } from "../shared/api/get_data";
+import { Spin } from "antd";
 
 function App() {
+    const [selectedLvl, setSelectedLvl] = useState<number>(1);
+    const [selectedPart, setSelectedPart] = useState<string>("명사");
+    const [data, setData] = useState<TWordData[] | null>(null);
+    const [isLoading, setIsLoading] = useState(false);
+    const [error, setError] = useState<string | null>(null);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            setIsLoading(true);
+            setError(null);
+            setData(null);
+
+            try {
+                const response = await getData(selectedPart, selectedLvl, 5);
+                setData(response);
+            } catch (err) {
+                console.error("Error loading data:", err);
+                setError("Error loading data");
+            } finally {
+                setIsLoading(false);
+            }
+        };
+
+        fetchData();
+    }, [selectedLvl, selectedPart]);
+
+    const handleLvlChange = (lvl: string) => {
+        setSelectedLvl(Number(lvl));
+    };
+
+    const handlePartChange = (part: string) => {
+        setSelectedPart(part);
+    };
+
     return (
-        <QueryClientProvider client={queryClient}>
-            <LvlBar />
-            <PartBar />
+        <>
+            <LvlBar onChange={handleLvlChange} />
+            <PartBar onChange={handlePartChange} />
+
             <div className="app-container">
-                {data.map((wordData, index) => (
-                    <CardWord key={index} currentWord={wordData} />
-                ))}
+                {isLoading && <Spin fullscreen />}
+
+                {error && <p>{error}</p>}
+
+                {data && data.length > 0
+                    ? data.map((wordData, index) => (
+                          <CardWord key={index} currentWord={wordData} />
+                      ))
+                    : !isLoading && <p>No data available</p>}
             </div>
-        </QueryClientProvider>
+        </>
     );
 }
 
